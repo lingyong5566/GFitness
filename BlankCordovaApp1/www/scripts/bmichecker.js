4170e3e32cb51ab8318d4f9258ded29dd0fa0ec3
ly <goh.lingyong@stee.stengg.com>
2019-01-24 18:39:24 +0800

    Commit

--- BlankCordovaApp1/www/scripts/bmichecker.js
+++ BlankCordovaApp1/www/scripts/bmichecker.js
@@ -1,5 +1,77 @@
 ﻿var app = angular.module('myApp', []);
 app.controller('fitnessCtrl', function ($scope, $http) {
+<<<<<<< HEAD
+    $scope.bmi = 0;
+
+    $scope.checker = true;
+    var calcForWomen = function () {
+        return 655 + (9.6 * parseFloat($scope.weight)) + (1.8 * parseFloat($scope.height) * 100) - (4.7 * parseFloat($scope.age));
+    }
+
+    var calcForMan = function () {
+        return 66 + (13.7 * parseFloat($scope.weight)) + (5 * parseFloat($scope.height) * 100) - (6.8 * parseFloat($scope.age));
+    }
+
+    var exerciseFreqNeeds = function (BMR, freq) {
+        var multiplier = [1.2, 1.375, 1.55, 1.725, 1.9];
+        return BMR * multiplier[freq];
+    }
+
+    var calcNoOfDays = function (difference) {
+        
+        var caloriesInKg = 0.064;
+        var noOfDays = Math.abs(difference) / caloriesInKg; 
+
+        return noOfDays;
+    }
+
+    $scope.back = function () {
+        $scope.checker = true;
+    }
+
+    $scope.height = 1.7;
+    $scope.weight = 65;
+    $scope.age = 6;
+    $scope.dWeight = 60;
+    $scope.gender = "F";
+    $scope.frequency = "0";
+    $scope.calculate = function () {
+
+        
+        $scope.bmi = parseFloat($scope.weight / (($scope.height) * ($scope.height))).toFixed(2);
+        //theChart.series[0].data = getData(parseInt($scope.bmi));
+        if ($scope.bmi < 18.5) {
+            $scope.results = "Underweight";
+        }
+        else if ($scope.bmi < 24.99) {
+            $scope.results = "Normal Weight";
+        }
+        else if ($scope.bmi < 29.99) {
+            $scope.results = "Overweight";
+        }
+        else if ($scope.bmi < 34.99) {
+            $scope.results = 'Obese (class 1)';
+        }
+        else if ($scope.bmi < 39.99) {
+            $scope.results = 'Obese (class 2)';
+        }
+        else {
+            $scope.results = "Morbidly Obese";
+        }
+
+        $scope.bmr = $scope.gender == "M" ? calcForMan() : calcForWomen();
+        $scope.bmr = parseFloat($scope.bmr).toFixed(2);
+        $scope.mWeight = parseFloat(exerciseFreqNeeds($scope.bmr, $scope.frequency)).toFixed(2);
+        $scope.tWeight = Math.abs(parseFloat($scope.dWeight) - parseFloat($scope.weight));
+        $scope.noOfDays = parseInt(calcNoOfDays($scope.tWeight));
+        $scope.checker = false;
+    }
+    $scope.goTo = function (location) {
+        window.location = location + ".html";
+    }
+});
+
+=======
 
     $scope.login = function () {
         console.log("Login called.");
@@ -43,3 +115,4 @@ app.controller('fitnessCtrl', function ($scope, $http) {
         window.location = location + ".html";
     }
 });
+>>>>>>> 5dbb39ea81973c49c2d01ed6ce237a41e41f6bdf
---
+++ BlankCordovaApp1/www/scripts/hiitplanner.js
@@ -0,0 +1,51 @@
+﻿var app = angular.module('myApp', []);
+app.controller('fitnessCtrl', function ($scope, $http) {
+    $scope.checker = true;
+
+    $scope.reps = [];
+    var timePerRep = function () {
+        return parseInt($scope.exerciseTime / $scope.repetition);
+    }
+
+    var workRestTime = function () {
+        var rep = timePerRep();
+        var repInSeconds = rep * 60;
+        var totalRatio = parseInt($scope.work) + parseInt($scope.rest);
+        console.log(repInSeconds);
+        console.log(totalRatio);
+        console.log($scope.work);
+        var workTime = parseInt(repInSeconds / totalRatio * $scope.work);
+        var restTime = parseInt(repInSeconds / totalRatio * $scope.rest);
+        return [workTime, restTime];
+    }
+    
+    $scope.plan = function () {
+        var workRestTotalTime = workRestTime();
+        $scope.reps = [];
+        console.log(workRestTime());
+        $scope.totalWorkTime = parseInt(workRestTotalTime[0] * parseInt($scope.repetition) / 60);
+        console.log(workRestTotalTime[0] * parseInt($scope.repetition));
+        $scope.totalRestTime = parseInt(workRestTotalTime[1] * parseInt($scope.repetition) / 60);
+        $scope.totalHIITTime = (parseInt($scope.totalWorkTime) + parseInt($scope.totalRestTime));
+        for (var x = 0; x < $scope.repetition; x++) {
+            $scope.reps.push({ "rep": x + 1, "work": parseInt(workRestTotalTime[0]), "rest": parseInt(workRestTotalTime[1]) });
+        }
+        console.log($scope.reps);
+        $scope.checker = false;
+    }
+
+    $scope.back = function () {
+        $scope.checker = true;
+    }
+
+    $scope.goTo = function (location) {
+        window.location = location + ".html";
+    }
+
+    $scope.exerciseTime = 60;
+    $scope.repetition = 10;
+    $scope.work = 2;
+    $scope.rest = 1;
+    $scope.exercise = "0";  
+});
+
